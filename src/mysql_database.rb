8ac77d549aca37b3cb6864779a1a13d23bc6f5de
Agus Kurniawan <agusk2007@gmail.com>
2014-01-06 10:52:14 +0100

    updated

--- src/mysql_database.rb
+++ src/mysql_database.rb
@@ -23,10 +23,25 @@ class MysqlDatabase
   end
 
   def create_data
+    puts('connecting to mysql server..')
+    mysql = Mysql.connect(@svr, @usr, @pwd, @db)
+    puts('connected')
+
+    puts('inserting 10 data...')
+    stmt = mysql.prepare('insert into product(name,price,created) values (?,?,now())')
+
+    i = 1
+    while i<=10
+      stmt.execute "product #{i}",0.34*i
+      i += 1
+    end
+    puts('done')
+    mysql.close()
 
   end
 
   def read_data
+
 
   end
 
@@ -44,3 +59,4 @@ end
 
 db = MysqlDatabase.new('127.0.0.1','mydatabase','user','password')
 db.connect()
+db.create_data()
