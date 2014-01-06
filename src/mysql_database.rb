756118485835b66487c4ca248f60df5e3fbebb24
Agus Kurniawan <agusk2007@gmail.com>
2014-01-06 11:15:21 +0100

    updated

--- src/mysql_database.rb
+++ src/mysql_database.rb
@@ -36,23 +36,46 @@ class MysqlDatabase
     end
     puts('done')
     mysql.close()
-
   end
 
   def read_data
+    puts('connecting to mysql server..')
+    mysql = Mysql.connect(@svr, @usr, @pwd, @db)
+    puts('connected')
 
+    puts('retrieving all data')
+    results = mysql.query('select idproduct,name,price,created from product')
+    results.each do |idproduct,name,price,created|
+      puts("#{idproduct}\t #{name}\t #{price}\t #{created}")
+    end
+    mysql.close()
 
   end
 
-  def update_data
+  def update_data(id)
+    puts('connecting to mysql server..')
+    mysql = Mysql.connect(@svr, @usr, @pwd, @db)
+    puts('connected')
 
+    puts("updating data id #{id}")
+    stmt = mysql.prepare('update product set name=?,price=? where idproduct=?')
+    # you can change these values
+    stmt.execute 'product-updated',0.75,id
+    puts('done')
+    mysql.close()
   end
 
   def delete_data(id)
+    puts('connecting to mysql server..')
+    mysql = Mysql.connect(@svr, @usr, @pwd, @db)
+    puts('connected')
 
-
+    puts("deleting data id #{id}")
+    stmt = mysql.prepare('delete from product where idproduct=?')
+    stmt.execute id
+    puts('done')
+    mysql.close()
   end
-
 
 end
 
@@ -60,3 +83,7 @@ end
 db = MysqlDatabase.new('127.0.0.1','mydatabase','user','password')
 db.connect()
 db.create_data()
+db.read_data()
+db.update_data(24) # change this value
+db.delete_data(25) # change this value
+db.read_data()
